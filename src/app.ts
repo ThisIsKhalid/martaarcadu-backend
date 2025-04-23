ceb74e996ee6e569049b215624cd932f9d04fd97
ThisIsKhalid <iamkhalid.hs@gmail.com>
2025-04-23 17:39:07 +0600

    Add user interface, logging, and health profile models; update environment and configuration

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import path from "path";
 import bodyParser from "body-parser";
 import router from "./app/routes";
 import GlobalErrorHandler from "./app/middlewares/globalErrorHandler";
+import logger from "./utils/logger";
+
 
 const app: Application = express();
 
@@ -30,15 +32,29 @@ app.use(express.static("public"));
 // Route handler for the root endpoint
 app.get("/", (req: Request, res: Response) => {
   res.send({
-    message: "How's Project API",
+    message: "Welcome to the API",
   });
 });
 
 // app.use("/uploads", express.static(path.join("/var/www/uploads")));
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads'))); // Serve static files from the "uploads" directory
 
+// Log incoming requests
+app.use((req: Request, res: Response, next: NextFunction) => {
+  logger.info(`Incoming request: ${req.method} ${req.originalUrl}`);
+  next();
+});
+
 // Setup API routes
 app.use("/api/v1", router);
+
+// health check
+app.get("/api/v1/health", (req: Request, res: Response) => {
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "Server is healthy",
+  });
+});
 
 // Error handling middleware
 app.use(GlobalErrorHandler);
