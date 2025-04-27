ed0a1b2701623241702b8639e0729a62d15bb865
zihad868 <zihadh868@gmail.com>
2025-04-27 18:19:23 +0600

    Implement appointment

--- src/app/modules/Appointment/appointment.routes.ts
+++ src/app/modules/Appointment/appointment.routes.ts
@@ -4,5 +4,6 @@ import { AppointmentController } from "./appointment.controller";
 const router = express.Router();
 
 router.post('/', AppointmentController.createAppointment);
+router.get("/:id", AppointmentController.partnarAppointment);
 
 export const AppointmentRoutes = router;
--- src/app/modules/Appointment/appointment.service.ts
+++ src/app/modules/Appointment/appointment.service.ts
@@ -17,15 +17,13 @@ const createAppointment = async (appointmentData: IAppointment) => {
 
   const patientExists = await prisma.user.findUnique({
     where: {
-        id: appointmentData.patientId
-    }
-  })
+      id: appointmentData.patientId,
+    },
+  });
 
-  if(!patientExists){
+  if (!patientExists) {
     throw new ApiError(httpStatus.NOT_FOUND, "Patient not found");
   }
-
-
 
   const appointment = await prisma.appointment.create({
     data: {
@@ -44,6 +42,86 @@ const createAppointment = async (appointmentData: IAppointment) => {
   return appointment;
 };
 
+const getPartnarAppointment = async (
+  id: string,
+  filters: {
+    searchTerm?: string;
+  },
+  options: IPaginationOptions
+) => {
+
+  const partner = await prisma.partner.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!partner) {
+    throw new ApiError(httpStatus.NOT_FOUND, "Partner not found");
+  }
+
+
+  const patient = await prisma.user.findUnique({
+    where: {
+      id
+    }
+  })
+
+  if(!patient){
+    throw new ApiError(httpStatus.NOT_FOUND, "Patient not found");
+  }
+
+  const { searchTerm } = filters;
+  const { page, skip, limit, sortBy, sortOrder } =
+    paginationHelpers.calculatePagination(options);
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      OR: ["partnerId", "patientId", "status", "timeSlot"].map((field) => ({
+        [field]: {
+          contains: searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  const whereConditions: Prisma.ProductWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const product = await prisma.product.findMany({
+    where: {
+      ...whereConditions,
+    },
+    skip,
+    take: limit,
+    orderBy:
+      sortBy && sortOrder
+        ? { [sortBy]: sortOrder }
+        : {
+            createdAt: "desc",
+          },
+  });
+
+  const total = await prisma.product.count({
+    where: {
+      ...whereConditions,
+    },
+  });
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: product,
+  };
+};
+
 export const AppointmentService = {
   createAppointment,
+  getPartnarAppointment,
 };
